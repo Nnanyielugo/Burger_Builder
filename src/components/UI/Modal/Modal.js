5d24131e380979bd4317c8f6288351cf09108950
Nnanyielugo <kz.onyegbule@gmail.com>
2018-02-23 16:54:34 +0100

    changed folder structure and used update lifecycle methkd to conditionally re-render

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div 
-      className={classes.Modal} 
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh',
-        opacity: props.show ? '1' : '0'
-        }}>
-      {props.children}
-    </div>
-  </Aux>
-)
+class Modal extends Component {
 
-export default modal
+  /* since Modal show property is dependent on the boolean state 'purchasing', and is set to either true or false,
+   shouldComponentUpdate checks if the nextProps 'show' property has changed, and returns a boolean.
+   If the boolean returns true, the component updates, and does not update otherwise.
+
+   This is strictly for performance sake, to avoid component re-rendering.
+   Since Modal wraps Order Summary, Modal's shouldComponentDidUpdate also controls Order Summary's rendering.
+
+   We could have used PureComponent, but since it does more checks than we want to, we're manually doing the checks.
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+// re-rendering checks
+  componentWillUpdate(){
+    console.log('[Modal] will update')
+    // alert('Component will update')
+  }
+
+  componentDidUpdate(){
+    // alert('Component has updated')
+    console.log('Component has updated')
+  }
+
+  render() {
+    console.log('Rendering')
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div 
+          className={classes.Modal} 
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh',
+            opacity: this.props.show ? '1' : '0'
+            }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }  
+}
+
+export default Modal
