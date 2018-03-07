21fdf25c0c5b04d768d561aec0600bc418a902b9
Nnanyielugo <kz.onyegbule@gmail.com>
2018-03-07 15:07:02 +0100

    form, configuration, state, and submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,62 @@ import Button from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css';
 import axios from '../../../axios-orders'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
+  // configure javascript form data in state
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalcode: ''
+    orderForm: {      
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },       
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIPCODE'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },       
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your E-Mail'
+        },
+        value: ''
+      },      
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
   }
@@ -22,19 +70,19 @@ class ContactData extends Component {
     this.setState({loading: true})
     // alert('You continue!')
     // appending '.json' to the endpoint url is firebase specific
+    const formData = {};
+    console.log(this.state.orderForm);
+    // forlElementIdentifier = name, address, etc
+    // then access the value property in each element identifier
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
+
+    // console.log(formData)
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'Kizito Onyegbule',
-        address: {
-          street: 'Agungi',
-          zipCode: 67684,
-          country: 'Nigeria'
-        },
-        email: 'test@email.com'
-      },
-      deliveryMethod: 'fastest'
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -47,13 +95,41 @@ class ContactData extends Component {
 
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    // console.log(event.target.value)
+    // make copy of state
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    // make a deeper copy of state, accessing the input identifier
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }
+
   render() {
+    const formElementsArray = [];
+    // loop through orderform keys (name, street, etc)
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
     let form = (
-      <form>
-        <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-        <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-        <input className={classes.Input} type="text" name="street" placeholder="Street" />
-        <input className={classes.Input} type="text" name="postalcode" placeholder="Postal Code" />
+      // use onSubmit handler on form instead of onClick handler on button for the submit event
+      <form onSubmit={this.orderHandler} >
+        {formElementsArray.map(formElement => (
+          <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
